be0017ea1f1d94e44b77a6756e7d28816c6f15eb
joule28nic <153733673+joule28nic@users.noreply.github.com>
2024-02-03 14:38:50 +0000

    24/2/3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,27 @@
 class BooksController < ApplicationController
-  
+
   def create
-    
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(book_params)
   end
 
   def edit
   end
-  
+
+  private
+
+  def book_params
+    params.permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
   def index
   end
 
   def show
-    @user = User.find(params[:id])
-    @book = Book.new
-    @books = @user.books
   end
 
   def edit
